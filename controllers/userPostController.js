fbe9775d1037010e5e80599151a493161adb73b6
ucefez7 <yousafalieamonu@gmail.com>
2024-11-06 13:19:14 +0530

    commit b4 discovery change in get all post

--- controllers/userPostController.js
+++ controllers/userPostController.js
@@ -331,10 +331,52 @@ exports.likePost = async (req, res) => {
 
 
 
-// Get all posts from every user
+// // Get all posts from every user
+// exports.getAllPosts = async (req, res) => {
+//   try {
+    
+    
+//     const posts = await Post.find({
+//       isBlocked: false,
+//       isArchived: false,
+//     }).populate("user", "name username profileImg");
+
+//     if (!posts.length) {
+//       return res.status(404).json({ message: "No posts found" });
+//     }
+
+
+//     const postsWithFriendshipStatus = await Promise.all(posts.map(async (post) => {
+//       const friendship = await Friendship.findOne({
+//         $or: [
+//           { requester: req.user.id, recipient: post.id },
+//           { requester: post.id, recipient: req.user.id }
+//         ]
+//       });
+
+//       let friendshipStatus = 'none';
+//       if (friendship) {
+//         friendshipStatus = friendship.status === 'accepted' ? 'looped' :
+//                            friendship.status === 'pending' ? 'requested' : 'none';
+//       }
+
+//       return { ...post.toObject(), friendshipStatus,};
+//     }));
+
+//     res.status(200).json({ data: postsWithFriendshipStatus, message: "Successful" });
+    
+//   } catch (error) {
+//     console.error("Error fetching all posts:", error);
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+
+
+
+// Get all posts
 exports.getAllPosts = async (req, res) => {
   try {
-    
     
     const posts = await Post.find({
       isBlocked: false,
@@ -345,15 +387,20 @@ exports.getAllPosts = async (req, res) => {
       return res.status(404).json({ message: "No posts found" });
     }
 
-
+    
     const postsWithFriendshipStatus = await Promise.all(posts.map(async (post) => {
+      if (!post.user) {
+        return null; 
+      }
+
       const friendship = await Friendship.findOne({
         $or: [
-          { requester: req.user.id, recipient: post.id },
-          { requester: post.id, recipient: req.user.id }
+          { requester: req.user.id, recipient: post.user._id },
+          { requester: post.user._id, recipient: req.user.id }
         ]
       });
 
+      
       let friendshipStatus = 'none';
       if (friendship) {
         friendshipStatus = friendship.status === 'accepted' ? 'looped' :
@@ -363,12 +410,19 @@ exports.getAllPosts = async (req, res) => {
       return { ...post.toObject(), friendshipStatus };
     }));
 
-    res.status(200).json({ data: postsWithFriendshipStatus, message: "Successful" });
+    
+    const acceptedFriendPosts = postsWithFriendshipStatus
+      .filter(post => post && post.friendshipStatus === 'looped');
+
+    res.status(200).json({ data: acceptedFriendPosts, message: "Successful" });
   } catch (error) {
     console.error("Error fetching all posts:", error);
     res.status(500).json({ message: error.message });
   }
 };
+
+
+
 
 // Get all posts of a specific user by user ID
 exports.getPostsByUserId = async (req, res) => {
