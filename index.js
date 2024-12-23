c03be1cc222b0cb79c56554129df88c55520fb78
ucefez7 <yousafalieamonu@gmail.com>
2024-12-23 16:12:20 +0530

    Port number changed

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ setupSocket(server); // Pass the server to the Socket.IO setup function
 setupSocket1(server);
 setupCronJobs();
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
