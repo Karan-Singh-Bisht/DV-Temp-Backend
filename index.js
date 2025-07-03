cfc1ce6db42bb6f45bb5b8f3c57dfc3bb1c2fa92
Karan <karansbisht7@gmail.com>
2025-07-03 21:22:40 +0530

    Update

--- index.js
+++ index.js
@@ -43,9 +43,7 @@ app.set("trust proxy", 1);
 app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://dv-admin-beta.vercel.app",
-  "https://dv-admin-git-main-karan-singh-bishts-projects.vercel.app/",
-  "https://dv-admin-8xwbqdhvs-karan-singh-bishts-projects.vercel.app/",
+  "https://dv-admin.netlify.app/",
 ];
 
 app.use(
